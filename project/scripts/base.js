eeb7c6106df48b497bd5c1102964611a0f376d28
xanderbholden <xanderbholden@gmail.com>
2024-10-29 23:06:29 -0600

    data

--- project/scripts/base.js
+++ project/scripts/base.js
@@ -32,7 +32,7 @@ class Footer_and_HamBurger {
         })
     }
 }
-//Class to get the api in each page without code it each time I want to
+
 class DisplayFetch {
 
     constructor(url){
@@ -65,7 +65,7 @@ class DisplayFetch {
 
 const generalData = {
 
-    json_cars : "https://raw.githubusercontent.com/EduardOrellana/wdd231/main/project/data/car-collection.json",
+    json_cars : "https://raw.githubusercontent.com/xanderbholden/wdd231/main/project/data/car-collection.json",
 
     api_weather(lat, lon, apiKey) {
         const baseURL = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
